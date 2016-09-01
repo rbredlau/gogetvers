a9414c894fcb4582ab58e07fb6ef1d9fafd061ad
Robert Bredlau <rbredlau@broadlux.com>
2016-08-31 17:24:49 -0700

    Basic functionality in bin/main.go

---
+++ bin/main.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	dousage(false)
+	doversion()
+	if len(os.Args) >= 2 {
+		domake()
+		dorebuild()
+		doconst()
+	}
+	dousage(true)
+
+	os.Exit(0)
+}
+
+func domake() {
+	if os.Args[1] != "make" {
+		return
+	}
+	fmt.Println("domake") // TODO RM
+	os.Exit(0)
+}
+
+func dorebuild() {
+	if os.Args[1] != "rebuild" {
+		return
+	}
+	fmt.Println("dorebuild") // TODO RM
+	os.Exit(0)
+}
+
+func doconst() {
+	if os.Args[1] != "const" {
+		return
+	}
+	fmt.Println("doconst") // TODO RM
+	os.Exit(0)
+}
+
+func doversion() {
+	for _, v := range os.Args {
+		if v == "--version" {
+			fmt.Printf("gogetvers version %v\n", "TODO: FILL ME IN")
+			os.Exit(0)
+		}
+	}
+}
+
+func dousage(force bool) {
+	usage := `
+gogetvers --version
+    Print version information.
+
+gogetvers [-h|--help]
+    Print help information.
+
+gogetvers make [PATH]
+    Create version information for golang package at PATH; or
+    in current directory if PATH is omitted.
+
+gogetvers rebuild -f MANIFEST [PATH]
+    Rebuild package structure described by MANIFEST at PATH;
+    or in current directory if PATH is omitted.
+
+gogetvers const [-f FILE] [PATH]
+    Create a go source file with version information at PATH
+    if provided or in current directory otherwise.  FILE can
+    be used to specify the file name; if omitted file will
+    be named generated_gogetvers.go.
+`
+	for _, v := range os.Args {
+		if v == "-h" || v == "--help" {
+			fmt.Printf(usage)
+			os.Exit(0)
+		}
+	}
+	if len(os.Args) <= 1 || force {
+		fmt.Printf(usage)
+		os.Exit(0)
+	}
+}
---
+++ make.go
@@ -0,0 +1,10 @@
+package gogetvers
+
+import (
+	"fmt"
+)
+
+func Make(sourceDir string) error {
+	fmt.Println("Make!", sourceDir)
+	return nil
+}

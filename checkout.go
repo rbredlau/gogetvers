7984cc6bac2b069f8f42be8036512e64a0c76bf7
Robert Bredlau <rbredlau@broadlux.com>
2016-09-06 22:34:46 -0700

    Adding checkout command

--- checkout.go
+++ checkout.go
@@ -1,7 +1,7 @@
 package gogetvers
 
 import (
-	"errors"
+	_ "errors"
 	"io"
 	"path/filepath"
 )
@@ -20,16 +20,21 @@ func Checkout(outputDir, inputFile string, statusWriter io.Writer) error {
 	//
 	// A local type to record each git, the current status, etc.
 	type gitstat struct {
-		wanted    *GitInfo // What we want, i.e. what's in the manifest.
-		current   *GitInfo // What it currently is.
-		dirExists bool     // If wanted directory exists.
+		wanted       *GitInfo // What we want, i.e. what's in the manifest.
+		current      *GitInfo // What it currently is.
+		dirExists    bool     // If wanted directory exists.
+		dir          string
+		parentDir    string
+		switchBranch bool
+		switchHash   bool
+		gitClone     bool
 	}
 	//
 	// Gather a summary of operations to perform by checking each wanted git.
 	sw.Write("Gathering summary...")
 	stats := make(map[string]*gitstat) // Gits by home directory.
 	for _, v := range ser.Gits {
-		stats[v.HomeDir] = &gitstat{wanted: v, dirExists: false}
+		stats[v.HomeDir] = &gitstat{wanted: v, dirExists: false, switchBranch: false, switchHash: false, gitClone: false}
 	}
 	stats[ser.TargetGit.HomeDir] = &gitstat{wanted: ser.TargetGit}
 	gitsWithMods := []string{} // gits with local modifications
@@ -54,6 +59,8 @@ func Checkout(outputDir, inputFile string, statusWriter io.Writer) error {
 		} else {
 			v.dirExists = false
 		}
+		v.dir = abs
+		v.parentDir = filepath.Dir(abs)
 	}
 	//
 	// If either gitsWithMods or dirsNotGits is non-empty then abort.
@@ -74,14 +81,36 @@ func Checkout(outputDir, inputFile string, statusWriter io.Writer) error {
 			}
 			sw.Outdent()
 		}
-		return errors.New("Existing errors prevent further execution.")
+		//		return errors.New("Existing errors prevent further execution.") // TODO PUT ME BACK IN
 	}
 	sw.Writeln("done")
 	//
 	// Print the summary
+	sw.Writeln("What will be done:")
+	sw.Indent()
 	for _, stat := range stats {
-		sw.WriteGitInfo(stat.wanted)
+		if stat.dirExists {
+			sw.Writeln(stat.dir)
+			sw.Indent()
+			if stat.wanted.Branch == stat.current.Branch && stat.wanted.Hash == stat.current.Hash {
+				sw.Writeln("branch and hash are current; nothing to do")
+			} else {
+				if stat.wanted.Branch != stat.current.Branch {
+					sw.Printf("switch branch from %v to %v\n", stat.current.Branch, stat.wanted.Branch)
+					stat.switchBranch = true
+				}
+				if stat.wanted.Hash != stat.current.Hash {
+					sw.Printf("switch hash from %v to %v\n", stat.current.Hash, stat.wanted.Hash)
+					stat.switchHash = true
+				}
+			}
+			sw.Outdent()
+		} else {
+			sw.Printf("%v will be cloned\n", stat.dir)
+			stat.gitClone = true
+		}
 	}
+	sw.Outdent()
 	//
 	sw.Writeln("Performing checkout...")
 	sw.Indent()

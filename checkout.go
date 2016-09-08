aecb403c35448d90b5b72a5b2e458db318e1f2f4
Robert Bredlau <rbredlau@broadlux.com>
2016-09-07 17:14:05 -0700

    Adding checkout command

--- checkout.go
+++ checkout.go
@@ -133,7 +133,7 @@ func Checkout(outputDir, inputFile string, statusWriter io.Writer) error {
 			}
 		}
 		if stat.gitClone {
-			sw.Printf("git clone -b %v %v %v\n", stat.wanted.Branch, stat.wanted.OriginUrl, filepath.Base(stat.wanted.HomeDir))
+			sw.Printf("git clone -b master %v %v\n", stat.wanted.OriginUrl, filepath.Base(stat.wanted.HomeDir))
 			code, _, err := ExecProgram(stat.parentDir, "git", []string{"clone", "-b", stat.wanted.Branch, stat.wanted.OriginUrl, filepath.Base(stat.wanted.HomeDir)})
 			if err != nil {
 				sw.Error(err)

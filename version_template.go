5aad8084990b43cdab42b1634254715cddec849f
Robert Bredlau <rbredlau@broadlux.com>
2016-09-09 21:13:11 -0700

    Implementing const command

--- version_template.go
+++ version_template.go
@@ -3,7 +3,7 @@ package gogetvers
 const version_template = `
 package $PACKAGE_NAME
 
-const $CONSTANT_NAME = &$TYPE_NAME{$VERSION,[]struct{
+const $CONSTANT_NAME = $TYPE_NAME{$VERSION,[]struct{
 	Name string
 	Version string
 } $DEPENDENCIES}

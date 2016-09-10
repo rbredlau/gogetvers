edab0df16505d615c59d2eae918f04f815506330
Robert Bredlau <rbredlau@broadlux.com>
2016-09-09 21:46:57 -0700

    Add version info

--- version_template.go
+++ version_template.go
@@ -3,6 +3,10 @@ package gogetvers
 const version_template = `
 package $PACKAGE_NAME
 
+import(
+	"strings"
+)
+
 var $VARNAME = $TYPE_NAME{$VERSION,[]struct{
 	Name string
 	Version string
@@ -14,5 +18,21 @@ type $TYPE_NAME struct {
 		Name string
 		Version string
 	}
+}
+
+func (v $TYPE_NAME) Version(binaryName string) string {
+	return binaryName + " version " + v.Version
+}
+
+func (v $TYPE_NAME) VersionVerbose(binaryName string) string {
+	v := v.Version(binaryName) + "\n"
+	deps := []string{}
+	for _,dep:=range v.Dependencies {
+		deps = append(deps,dep.Name +" version " +dep.Version)
+	}
+	if len(deps)>0{
+		v = v + "    " + strings.Join(deps,"\n    ")
+	}
+	return v
 }
 `

fd8969bcc09c79a1682a13ec990a2176a2675311
Robert Bredlau <rbredlau@broadlux.com>
2016-09-13 21:45:25 -0700

    Fix golint issues in git_list.go

--- git_list.go
+++ git_list.go
@@ -5,16 +5,22 @@ import (
 	"strings"
 )
 
+// A utility type to sort Git types.
 type GitList []*Git
 
+// Create a new GitList
 func NewGitList(item ...*Git) GitList {
 	return append([]*Git{}, item...)
 }
 
+// Satisfies Sort interface.
 func (gl GitList) Len() int {
 	return len(gl)
 }
 
+// Satisfies Sort interface; one Git type is considered less
+// than another if it is higher up in the file system or
+// alphabetically if they have the same parent folder.
 func (gl GitList) Less(i, j int) bool {
 	gl[i].PathsToSlash()
 	gl[j].PathsToSlash()
@@ -31,6 +37,7 @@ func (gl GitList) Less(i, j int) bool {
 	return b < y
 }
 
+// Returns a list of all HomeDir for the gits in the list.
 func (gl GitList) Names() []string {
 	rv := []string{}
 	for _, v := range gl {
@@ -39,10 +46,12 @@ func (gl GitList) Names() []string {
 	return rv
 }
 
+// Sorts the list.
 func (gl GitList) Sort() {
 	sort.Sort(gl)
 }
 
+// Satisfies Sort interface.
 func (gl GitList) Swap(i, j int) {
 	gl[i], gl[j] = gl[j], gl[i]
 }

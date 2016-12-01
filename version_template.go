714783426a4fcc080a5d5a54680227e161f4bcec
Robert Bredlau <rbredlau@broadlux.com>
2016-11-30 19:14:31 -0800

    Remove Versioner interface.

--- version_template.go
+++ version_template.go
@@ -1,10 +1,5 @@
 package gogetvers
 
-type Versioner interface {
-	GetVersion(binaryName string) string
-	GetVersionVerbose(binaryName string) string
-}
-
 const versionTemplate = `
 package $PACKAGE_NAME
 
